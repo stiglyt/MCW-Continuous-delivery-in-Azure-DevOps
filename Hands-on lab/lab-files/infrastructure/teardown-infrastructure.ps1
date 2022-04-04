0a26d9aa9872cbacab34c103ae3898e001d85229
stiglyt <stig.lytjohan@gmail.com>
2022-04-04 20:13:26 +0200

    Update teardown-infrastructure.ps1

--- Hands-on lab/lab-files/infrastructure/teardown-infrastructure.ps1
+++ Hands-on lab/lab-files/infrastructure/teardown-infrastructure.ps1
@@ -1,6 +1,7 @@
 $studentprefix ="Your 3 letter abbreviation here"
 $resourcegroupName = "fabmedical-rg-" + $studentprefix
 
-az ad sp delete --id "fabmedical-$studentprefix"
+$spobjectid = az ad sp list --display-name "fabmedical-$studentprefix" --query "[0].objectId" -o tsv 
+az ad sp delete --id $spobjectid
 az group delete --name $resourceGroupName
 
